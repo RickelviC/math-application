217695e51d02fddf93569214c72f09306f74b03f
RickelviC <Cruzrickelvi@gmail.com>
2026-04-08 11:07:22 -0400

    added more math classes and looked up how to use Math.hypot

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -16,7 +16,41 @@ public class MathApp {
 
         double smallestPrice = Math.min(carPrice,truckPrice);
 
-        System.out.println( "The smallest price is " + smallestPrice);
+        System.out.println("The smalllest price is " + smallestPrice);
+
+
+        System.out.println("==========================================");
+
+        double radius = 7.25;
+        double area;
+
+        area = Math.pow(radius , 2) * Math.PI;
+
+        System.out.println("The area of a circle is " + area);
+
+        System.out.println("==========================================");
+
+        double square = Math.sqrt(5);
+
+        System.out.println("The square root is " + square);
+
+        System.out.println("==========================================");
+
+        double distance = Math.sqrt(Math.pow((5 - 85), 2) + Math.pow(( 10 - 50), 2));
+
+        // Source - https://stackoverflow.com/a/32047473
+        // Posted by Bob, modified by community. See post 'Timeline' for change history
+        // Retrieved 2026-04-08, License - CC BY-SA 3.0
+
+        // double distance = Math.hypot(5 - 85, 10 - 50);
+
+        System.out.println(distance);
+
+
+
+
+
+
 
 
 
