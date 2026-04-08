8dde66ee9b142323802e1f7a3da603121bc7121a
RickelviC <Cruzrickelvi@gmail.com>
2026-04-08 10:15:30 -0400

    added question 1 and 2

--- src/main/java/com/pluralsight/MathApp.java
+++ src/main/java/com/pluralsight/MathApp.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        double bobSalary = 2000;
+        double garySalary = 1950;
+
+        double highestSalary = Math.max(bobSalary,garySalary);
+
+        System.out.println("The highest salary is " + highestSalary);
+
+        System.out.println("==========================================");
+
+        double carPrice = 5999.99;
+        double truckPrice = 7399.99;
+
+        double smallestPrice = Math.min(carPrice,truckPrice);
+
+        System.out.println( "The smallest price is " + smallestPrice);
+
+
+
+    }
 }
